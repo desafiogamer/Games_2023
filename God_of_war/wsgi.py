eaf606b42a140f37b4d2f9d724c9c295ba8dc30c
joao vitor <145628900+desafiogamer@users.noreply.github.com>
2023-10-31 18:23:28 -0300

    Update wsgi.py

--- God_of_war/wsgi.py
+++ God_of_war/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'God_of_war.settings')
 
 application = get_wsgi_application()
+
+app = application
