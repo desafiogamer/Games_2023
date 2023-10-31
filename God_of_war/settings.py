97ff8b46cf790469fc36d3453b8fb77927c90fec
joao vitor <145628900+desafiogamer@users.noreply.github.com>
2023-10-31 17:31:50 -0300

    Update settings.py

--- God_of_war/settings.py
+++ God_of_war/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9c(urh&=f(bzsste3xu=oc@nhm99cq86^es67is)xq*k(v^r$-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
